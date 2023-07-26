1acbb860aba11c21b8929175f65223ac3296a008
Abdullah Ahmed <52929221+abdulahmd@users.noreply.github.com>
2023-07-26 17:17:41 -0500

    tab addition along with divs for content

--- robotics-react/src/App.js
+++ robotics-react/src/App.js
@@ -6,20 +6,33 @@ import Typewriter from "typewriter-effect";
 function App() {
   
   return (
-      <div className="banner">
-        <img className = "logo" alt = "logo" src={Independence} />
-          <div className = "title">
+    <div className="content">
+      <div className = "banner">
+        <div className="tabs">
+          <nav>
+            <a href=".Home">Home&nbsp;&nbsp;&nbsp;</a>
+            <a href=".About">About&nbsp;&nbsp;&nbsp;</a>
+            <a href=".Contact">Contact&nbsp;&nbsp;&nbsp;&nbsp;</a>
+          </nav>
+        </div>
+
+          <img className="logo" alt="logo" src={Independence} />
+          <div className="title">
             <Typewriter
-                    onInit={(typewriter) => {
-                        typewriter
-                            .typeString("| INDEPENDENCE ROBOTICS")
-                            .pauseFor(1000)
-                            .start();
-                    }}
-            />
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("| OVERDR!VE ROBOTICS")
+                  .pauseFor(1000)
+                  .start();
+              } } />
           </div>
-      </div>
-      
+        </div>
+
+        <div className="Home"></div>
+        <div className="About"></div>
+        <div className="Contact"></div>
+    </div>
   );
 }
+
 export default App;
