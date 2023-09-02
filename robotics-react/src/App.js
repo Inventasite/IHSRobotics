f0439a93f7e4a4f0347a8e03dadc3b8a213036b2
Abdullah Ahmed <52929221+abdulahmd@users.noreply.github.com>
2023-09-02 16:52:08 -0500

    foundation

--- robotics-react/src/App.js
+++ robotics-react/src/App.js
@@ -1,55 +1,70 @@
 import React from 'react';
 import './App.css';
 import Overdrive from './image (1).png';
-import Typewriter from "typewriter-effect";
+import Typewriter from 'typewriter-effect';
 
-function App() {
-  
+const App = () => {
+  // Generate an array of triangles
+  const triangles = Array.from({ length: 50 }, (_, index) => (
+    <div
+      key={index}
+      className="triangle-background"
+      style={{
+        '--index': index,
+        '--position': Math.random(),
+      }}
+    />
+  ));
+
   return (
     <div className="content">
-      <div className = "banner">
+      {triangles} {/* Render the spinning triangles */}
+      <div className="banner">
         <div className="tabs">
           <nav>
-            <a href=".Home">Home&nbsp;&nbsp;&nbsp;</a>
-            <a href=".About">About&nbsp;&nbsp;&nbsp;</a>
-            <a href=".Contact">Contact&nbsp;&nbsp;&nbsp;&nbsp;</a>
-          </ nav>
+            <a href="#Home" className="tabLink">
+              Home
+            </a>
+            <a href="#About" className="tabLink">
+              About
+            </a>
+            <a href="#Contact" className="tabLink">
+              Contact
+            </a>
+          </nav>
         </div>
 
-          <img className="logo" alt="logo" src={Overdrive} />
-          <div className="title">
-            <Typewriter
-              onInit={(typewriter) => {
-                typewriter
-                  .typeString("OVERDR!VE ROBOTICS")
-                  .pauseFor(1000)
-                  .start();
-              } } />
-          </div>
+        <img className="logo" alt="logo" src={Overdrive} />
+        <div className="title">
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter.typeString('OVERDR!VE ROBOTICS').pauseFor(1000).start();
+            }}
+          />
         </div>
+      </div>
 
-        <h2 className="homeTitle">H O M E</h2>
-        <div className="Home">
-          <br></br>
-          <p>Here are our team members</p>
-          <p>Here are our awards</p>
-        </div>
+      <h2 className="homeTitle">H O M E</h2>
+      <div id="Home" className="Home">
+        <br />
+        <p>Here are our team members</p>
+        <p>Here are our awards</p>
+      </div>
 
-        <h2 className = "aboutTitle">A B O U T</h2>
-        <div className="About">
-          <br></br>
-          <p>Here are our robots</p>
+      <h2 className="aboutTitle">A B O U T</h2>
+      <div id="About" className="About">
+        <br />
+        <p>Here are our robots</p>
+      </div>
 
-        </div>
-
-        <h2 className = "contactTitle">C O N T A C T</h2>
-        <div className="Contact">
-          <br></br>
-          <p>Here's our interest form</p>
-          <p>Contact form here</p>
-        </div>
+      <h2 className="contactTitle">C O N T A C T</h2>
+      <div id="Contact" className="Contact">
+        <br />
+        <p>Here's our interest form</p>
+        <p>Contact form here</p>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
