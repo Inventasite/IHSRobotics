18255b8ba729245f981d4b350ffd2b44e13a910a
Abdullah Ahmed <52929221+abdulahmd@users.noreply.github.com>
2023-09-02 17:20:36 -0500

    Updates

---
+++ precache-manifest.0d65fed10b980c834ec71d0a672e6e05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea560057b57b9c53a5f5",
+    "url": "/IHSRobotics/static/css/main.a0775c34.chunk.css"
+  },
+  {
+    "revision": "ea560057b57b9c53a5f5",
+    "url": "/IHSRobotics/static/js/main.ea560057.chunk.js"
+  },
+  {
+    "revision": "3f5a8f4c7bc0532f173e",
+    "url": "/IHSRobotics/static/js/1.3f5a8f4c.chunk.js"
+  },
+  {
+    "revision": "d5845aa9f2bc81a9d0e1",
+    "url": "/IHSRobotics/static/js/2.d5845aa9.chunk.js"
+  },
+  {
+    "revision": "8e569147e45d31cd9566",
+    "url": "/IHSRobotics/static/js/runtime~main.8e569147.js"
+  },
+  {
+    "revision": "042f1619c5a748c3ff1bd0f87d12dbc9",
+    "url": "/IHSRobotics/static/media/image (1).042f1619.png"
+  },
+  {
+    "revision": "ae96f41c84fa3991bba77dd0980addd2",
+    "url": "/IHSRobotics/index.html"
+  }
+];
